8f04183f1ea9e445df1ea58456999160e25bce90
Julien Kasarherou <julien@openio.io>
2019-09-09 15:19:31 +0900

    workaround for roundtrip

--- oio/bootstrap.sh
+++ oio/bootstrap.sh
@@ -7,11 +7,10 @@ set -o pipefail
 
 
 bootstrap_openio() {
+  openio cluster wait -u oioproxy rawx rdir meta2 meta1 meta0 account -n 1 -f json
   openio directory bootstrap --check --replicas 1
   openio rdir bootstrap rawx -f json
   openio rdir bootstrap meta2 -f json
 }
-
-wait_for_openio
 
 bootstrap_openio
--- oio/roundtrip.sh
+++ oio/roundtrip.sh
@@ -2,16 +2,42 @@
 
 set -o nounset
 set -o pipefail
-set -o errexit
+#set -o errexit
 
 . /app/.oio/env.sh
 
 
+wait_for_openio() {
+  openio cluster wait -u oioproxy rawx rdir meta2 meta1 meta0 account -n 1 -f json
+
+  local max=5
+  local sleep_time=1
+  local attempt=1
+  while [ $attempt -le $max ]; do
+    openio object create 1 /etc/magic -f json
+    status=$?
+    if [ $status -eq 0 ]; then
+        break
+    fi
+    echo "Waiting for openio ... ${attempt}/${max}"
+    attempt=$(($attempt+1))
+    sleep $sleep_time
+    if [ $attempt -gt $max ]; then
+      echo "Timeout!"
+      $GRIDINIT_CMD status2
+      exit 1
+    fi
+  done
+}
+
+
 test_openio() {
   openio cluster show -f json
-  openio container create 1 -f json
-  openio container show 1 -f json
-  openio object create 1 /etc/magic -f json -v --debug
+  openio container create roundtrip -f json
+  openio container show roundtrip -f json
+  openio object create roundtrip /etc/os-release -f json
+  openio object show roundtrip os-release -f json
+  openio object delete roundtrip os-release -f json
 }
 
 test_oioswift() {
