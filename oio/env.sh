343cfdd220db0da4f3c2105fd56a0a5668f707d1
Julien Kasarherou <julien@openio.io>
2019-09-17 15:01:36 +0900

    change default account

--- oio/env.sh
+++ oio/env.sh
@@ -4,4 +4,4 @@ export PATH="/app/oio/bin:/app/venv/bin:$PATH"
 export LD_LIBRARY_PATH="/app/oio/lib"
 export PYTHONPATH="/app/venv/lib/python2.7/site-packages"
 export OIO_NS=QA
-export OIO_ACCOUNT=qa
+export OIO_ACCOUNT=AUTH_demo
